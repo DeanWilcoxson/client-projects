f8474e7d9427e24ee58827a6e27128a425ee0dfb
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2022-05-31 17:54:06 -0700

    5/31/22

--- oakdale-wt/src/App.js
+++ oakdale-wt/src/App.js
@@ -1,5 +1,12 @@
+import { Routes, Route } from "react-router-dom";
 const App = () => {
-  <div className="App"></div>;
+  <div className="App">
+    <Routes>
+      <Route></Route>
+      <Route></Route>
+      <Route></Route>
+    </Routes>
+  </div>;
 };
 
 export default App;
