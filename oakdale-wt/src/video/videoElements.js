65b0415b7c76f0e85cf7d310b8389ee8b0aec43e
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2022-06-01 15:54:16 -0700

    6/2/22

---
+++ oakdale-wt/src/video/videoElements.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const HeroBg = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  z-index: 0;
+`;
+export const VideoBg = styled.video`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  -o-object-fit: cover;
+  background: #232a34;
+  z-index: 0;
+`;
