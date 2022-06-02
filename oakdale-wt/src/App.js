741cb64a07a2648c987b8724acc6dcd11d31c0d7
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2022-06-01 23:12:01 -0700

    6/1/22

--- oakdale-wt/src/App.js
+++ oakdale-wt/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { VideoBg, HeroBg } from "./video/videoElements.js";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, } from "react-router-dom";
 import video from "./video/car-closeup.mp4";
 
 const App = () => {
