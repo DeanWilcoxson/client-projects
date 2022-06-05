cf469543f1f5b1217f1aaa872f7d20a005117829
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2022-06-05 10:11:13 -0700

    6/5/22

--- oakdale-wt/src/App.js
+++ oakdale-wt/src/App.js
@@ -1,23 +1,20 @@
 import React from "react";
 import Landing from "./components/global/landing-page";
-import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
-import Automotive from './components/parent/Automotive'
-import Residential from './components/parent/Residential'
-import Commercial from './components/parent/Commercial'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Automotive from "./components/parent/Automotive";
+import Residential from "./components/parent/Residential";
+import Commercial from "./components/parent/Commercial";
 
 const App = () => {
   return (
     <div>
       <Router>
         <Routes>
-          <Route path='/' element={<Landing />}/>
-          <Route path='/automotive' element={<Automotive />}/>
-          <Route path='/commercial' element={<Commercial />}/>
-          <Route path='/residential' element={<Residential />}/>
+          <Route path="/" element={<Landing />} />
+          <Route path="/automotive" element={<Automotive />} />
+          <Route path="/commercial" element={<Commercial />} />
+          <Route path="/residential" element={<Residential />} />
         </Routes>
-     
-          
-  
       </Router>
     </div>
   );
