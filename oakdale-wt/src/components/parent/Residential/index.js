6e4804b4eb1ee14beebc10e1736269752a02dba8
bryanhuffstutler <bhuff8404@gmail.com>
2022-06-05 13:57:32 -0500

    started Residential parent, with contact component

--- oakdale-wt/src/components/parent/Residential/index.js
+++ oakdale-wt/src/components/parent/Residential/index.js
@@ -1,5 +1,37 @@
+import React from 'react'
+import houseTint from '../../../images/HouseTint.jpg'
+import Sidebar from '../../global/sidebar'
+import ContactFormResidential from './ContactFormResidential'
+
 const Residential = () => {
-    return <h1>RESIDENTIAL PARENT</h1>
+    const links = [{
+        endpoint: '/commercial',
+        name: 'Commercial'
+    },
+    {
+        endpoint: '/residential',
+        name: "Residential"
+    },
+    {
+        endpoint: '/contact',
+        name: 'Contact'
+    }]
+
+    return (
+        <div>
+
+            {/* Sidebar needs to be added to grid on left side to encompass the entire left portion of page. */}
+            <Sidebar links = {links}/>
+
+            <h1>Residential</h1>
+
+            <img src={houseTint} style={{height: '700px', width: '950px'}}/>
+
+            <h3>Oakdale Window Tint</h3>
+
+            <ContactFormResidential />
+        </div>
+    )
 }
 
 export default Residential
