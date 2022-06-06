2a22f1e0d16d7d5ed0d1258c1b09a6c4b00b82c6
bryanhuffstutler <bhuff8404@gmail.com>
2022-06-05 19:06:29 -0500

    stage

--- oakdale-wt/src/App.js
+++ oakdale-wt/src/App.js
@@ -1,21 +1,22 @@
 import React from "react";
 import Landing from "./components/global/landing-page";
-import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Automotive from './components/parent/Automotive';
 import Residential from './components/parent/Residential';
 import Commercial from './components/parent/Commercial';
 import Error from './components/global/error'
 
 const App = () => {
+  
   return (
     <div>
       <Router>
         <Routes>
-          <Route path='/' element={<Landing />}/>
-          <Route path='/automotive' element={<Automotive />}/>
-          <Route path='/commercial' element={<Commercial />}/>
-          <Route path='/residential' element={<Residential />}/>
-          <Route path='*' element={<Error />}/>
+          <Route path='/' element={<Landing />} />
+          <Route path='/automotive' element={<Automotive />} />
+          <Route path='/commercial' element={<Commercial />} />
+          <Route path='/residential' element={<Residential />} />
+          <Route path='*' element={<Error />} />
         </Routes>
       </Router>
     </div>
