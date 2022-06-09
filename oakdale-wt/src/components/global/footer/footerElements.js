cd50247d053888bbf76f7fe2e670815cef2ffc28
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2022-06-09 12:35:28 -0700

    6/9/22

---
+++ oakdale-wt/src/components/global/footer/footerElements.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+// import { FaPhoneAlt } from "react-icons/fa";
+// import { MdMessage } from "react-icons/md";
+export const FooterContainer = styled.div`
+  left: 0px;
+  bottom: 0px;
+  margin: 0px;
+  padding: 0px;
+  width: 100%;
+  background-color: #252525;
+  position: fixed;
+`;
+export const FooterText = styled.p`
+  color: white;
+  text-align: center;
+  padding: 5px;
+`;
+export const FooterBold = styled.b`
+  color: darkgrey;
+  padding: 2px;
+  margin: 5px;
+`;
+export const FooterBusinessInfoBox = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+    margin: 1.25rem;
+
+`;
