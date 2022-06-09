a111c3a9f10c82e29fbe47beb6e16cfa80903046
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2022-06-09 12:42:49 -0700

    6/9/22

--- oakdale-wt/src/components/global/footer/footerElements.js
+++ oakdale-wt/src/components/global/footer/footerElements.js
@@ -23,6 +23,5 @@ export const FooterBold = styled.b`
 export const FooterBusinessInfoBox = styled.div`
   display: flex;
   justify-content: space-evenly;
-    margin: 1.25rem;
-
+  margin: 1.25rem;
 `;
