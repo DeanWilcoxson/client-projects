1ecccce05ed5986b96b5bbc7ce4ce639ffd5b649
bryanhuffstutler <bhuff8404@gmail.com>
2022-06-23 19:55:31 -0500

    added a mobile nav bar

--- oakdale-wt/src/components/global/topbar/TopBarElements.js
+++ oakdale-wt/src/components/global/topbar/TopBarElements.js
@@ -12,6 +12,38 @@ export const TopContainer = styled.div`
   position: sticky;
   z-index: 1000;
   background-color: white;
+  @media screen and (max-width: 400px) {
+    .above-400-width {
+      display: none;
+    }
+    
+    display: flex;
+    justify-content: end;
+    align-items: flex-end;
+
+    #mob-nav-icon {
+      padding: 10px;
+      color: black;
+      font-weight: 900;
+      font-size: 1.2em;
+      a {
+        text-decoration: none;
+        background-color: #FA6B0A;
+        margin: 6px;
+        padding: 10px;
+        border-radius: 10px;
+        text-align: center;
+        :visited {color: inherit}
+      }
+    }
+
+    #mob-nav-links {
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-start;
+      z-index: 1000;
+    }
+  }
 `;
 export const LandingLogo = styled.img`
   width: 20%;
--- oakdale-wt/src/components/global/topbar/index.js
+++ oakdale-wt/src/components/global/topbar/index.js
@@ -1,12 +1,27 @@
 import React from "react";
 import logo from "./../../../images/OakdaleWindowTintLogo.jpg";
 import { TopContainer, LandingLogo, HorizontalRule } from "./TopBarElements";
+import MobileNav from '../mobile-nav/MobileNav'
 
 export const TopBar = () => {
   return (
-    <TopContainer>
-      <LandingLogo src={logo} />
-      <HorizontalRule />
-    </TopContainer>
+    <div>
+      <TopContainer>
+        <div className="above-400-width">
+
+          <LandingLogo src={logo} />
+          <HorizontalRule />
+
+        </div>
+
+        <div className="400-width-and-below">
+          <MobileNav />
+        </div>
+      </TopContainer>
+    </div>
+
+
+
+
   );
 };
