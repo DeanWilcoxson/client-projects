a8baf3cfea5e97f4936da0bbb79377893fbffb73
Dean Wilcoxson <dmwhswsmw919216@gmail.com>
2022-07-11 16:08:11 -0700

    7/11/22

--- juicy-jays/src/Components/topbar/topbarElements.js
+++ juicy-jays/src/Components/topbar/topbarElements.js
@@ -1,7 +1,5 @@
 import styled from "styled-components";
 export const TopbarContainer = styled.div``;
-export const TopbarHeader = styled.h1`
-  color: black;
-`;
+export const TopbarHeader = styled.h1``;
 export const TopbarNav = styled.div``;
 export const TopbarLink = styled.a``;
