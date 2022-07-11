00614c57ce49ad625c94265304d3842596c7cff2
Dean Wilcoxson <dmwhswsmw919216@gmail.com>
2022-07-11 15:52:59 -0700

    Create React App

---
+++ juicy-jays/src/App.js
@@ -0,0 +1,6 @@
+import React from "react";
+const App = () => {
+  return <>Juicy Jays</>;
+};
+
+export default App;
---
+++ juicy-jays/src/Components/topbar/topbarElements.js
@@ -0,0 +1,4 @@
+import { styled } from "styled-components";
+export const TopbarContainer = styled.div``;
+export const TopbarNav = styled.div``;
+export const TopbarLink = styled.a``;
